1d359780bd72c2a804aef9e5a056f877c1d3ec42
fuadsmaili1993 <fuad.smaili@gmx.de>
2024-01-28 18:45:21 +0100

    app js change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         i was changed again!
+         Fuad smaili has sucessfully deployed this application
         </p>
         <a
           className="App-link"
